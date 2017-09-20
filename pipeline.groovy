f1b7a49ae0346f171022cf6c190c871044d0aa1f
Jeremy Kleiman <jkleiman@redhat.com>
2017-09-20 14:22:33 -0400

    add basic pipeline script

---
+++ pipeline.groovy
@@ -0,0 +1,42 @@
+#!/usr/bin/groovy
+
+node('master') {
+
+  env.NAMESPACE = readFile('/var/run/secrets/kubernetes.io/serviceaccount/namespace').trim()
+  env.TOKEN = readFile('/var/run/secrets/kubernetes.io/serviceaccount/token').trim()
+  env.OC_CMD = "oc --token=${env.TOKEN} --server=${ocpApiServer} --certificate-authority=/run/secrets/kubernetes.io/serviceaccount/ca.crt --namespace=${env.NAMESPACE}"
+
+  env.APP_NAME = "${env.JOB_NAME}".replaceAll(/-?pipeline-?/, '').replaceAll(/-?${env.NAMESPACE}-?/, '')
+  def projectBase = "${env.NAMESPACE}".replaceAll(/-dev/, '')
+  env.STAGE1 = "${projectBase}-dev"
+  env.STAGE2 = "${projectBase}-stage"
+  env.STAGE3 = "${projectBase}-prod"
+
+//  sh(returnStdout: true, script: "${env.OC_CMD} get is jenkins-slave-image-mgmt --template=\'{{ .status.dockerImageRepository }}\' -n openshift > /tmp/jenkins-slave-image-mgmt.out")
+//  env.SKOPEO_SLAVE_IMAGE = readFile('/tmp/jenkins-slave-image-mgmt.out').trim()
+//  println "${env.SKOPEO_SLAVE_IMAGE}"
+
+}
+
+
+
+node {
+    echo 'Build cotd'
+    stage('SCM Checkout') {
+      checkout scm
+      sh "orig=\$(pwd); cd \$(dirname ${pomFileLocation}); git describe --tags; cd \$orig"
+    }
+    
+}
+
+node {
+   echo 'Promote to val'
+}
+
+node {
+    echo 'Run test'
+}
+
+node {
+    echo 'Promote to prod'
+}
